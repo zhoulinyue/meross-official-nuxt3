c870109b201635b29160fa4e41a9d4f0e30bd755
zhoulinyue <zhoulinyue@meross.com>
2023-08-22 18:20:06 +0800

    feat: 尝试使用nuxt3 hooks

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,3 +4,15 @@
     <NuxtLink to="/list">进入列表页</NuxtLink>
   </div>
 </template>
+
+<script setup>
+  const foo = useFoo();
+  // *nuxt 常用 hook，不需要 import 就能用
+  const appConfig = useAppConfig();
+  console.log('111111111----', appConfig);
+
+  const { data, pending, error, refresh } = await useAsyncData(
+    'mountains',
+    () => $fetch('https://api.nuxt.js.dev/mountains')
+  )
+</script>
